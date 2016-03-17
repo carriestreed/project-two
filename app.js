3c62903b088f68e958f1d2c9ee76ccdc235bf337
Carrie Streed <cmstreed@gmail.com>
2016-03-17 00:49:09 -0400

    sorted by 'interestingness'

--- app.js
+++ app.js
@@ -5,11 +5,10 @@ document.addEventListener('DOMContentLoaded', function(event){
   document.querySelector('#user-sub').addEventListener('click', function(){
     console.log('clicked');
 
-    var query='https://api.flickr.com/services/rest/?format=json&method=flickr.photos.search&api_key=' + API_KEY + '&tags=' + userSearch.value;
+    var query='https://api.flickr.com/services/rest/?format=json&nojsoncallback=1&method=flickr.photos.search&api_key=' + API_KEY + '&sort=interestingness-desc&tags=' + userSearch.value;
     $.ajax({
       url: query,
-      dataType: 'jsonp',              //required or will fail
-      jsonp: 'jsoncallback',          //required or will fail
+      dataType: 'json',
       success: function(response){
         console.log(response);
 
