1a3c3ced796117c8c0924f2a07bfc7252e5d7c54
Carrie Streed <cmstreed@gmail.com>
2016-05-11 10:24:43 -0400

    broke it

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-require( 'dotenv' ).config();
-var dotenv = require('dotenv');
+require( 'dotenv' ).config()
+var dotenv = require('dotenv')
 
-var express    = require('express'),
-var PORT       = process.env.PORT || 3000,
+var express    = require('express')
+var PORT       = process.env.PORT || 3000
 var app        = express()
 
 app.use(express.static('./'))
