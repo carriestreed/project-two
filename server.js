81959375e2a9c2ffc4abb3b0d18289f173d5283b
Carrie Streed <cmstreed@gmail.com>
2016-05-11 01:27:00 -0400

    minor

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 var express = require('express'),
     PORT    = process.env.PORT || 3000,
-    app     = express();
+    app     = express()
 
-app.use(express.static('./'));
+app.use(express.static('./'))
 
-app.get('/', function(req, res){
-  res.sendFile('/index.html');
-});
-​
 app.listen(PORT, function () {
-  console.log('server is listening on port ' + PORT + "!");
+  console.log('server is listening on port ' + PORT + "!")
 });
